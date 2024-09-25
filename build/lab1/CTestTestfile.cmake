3aff0a43e206d3ef8a3b96296208f5d29cda1e44
utyfull <serg_misha1@mail.ru>
2024-09-25 18:46:33 +0000

    delete files

--- build/lab1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/OS_labs/lab1
-# Build directory: /workspaces/OS_labs/build/lab1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
